56f7b9a9375ede653c05ec78e6197a1a2d4bd223
katiamiyu <katiamiyu@gmail.com>
2023-11-02 06:47:02 +0000

    4-binary_tree_is_leaf.c

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks for leaf
+ * @node: node to check
+ * Return: 1 success, 0 failed
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if ((node->left == NULL) && (node->right == NULL))
+	{
+		return (1);
+	}
+
+	return (0);
+}
