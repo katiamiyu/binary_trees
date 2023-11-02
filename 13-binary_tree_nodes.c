b23e59d995e00770f5bb5d66973301143bc3556e
katiamiyu <katiamiyu@gmail.com>
2023-11-02 12:34:26 +0000

    13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - non-leaf nodes count
+ * @tree: root
+ *
+ * Return: count
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree && (tree->left || tree->right))
+		return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
+
+	return (0);
+}
