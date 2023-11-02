9028a05c75db0d6424de8d0f0765ac6a36438bab
katiamiyu <katiamiyu@gmail.com>
2023-11-02 07:48:16 +0000

    8-binary_tree_postorder.c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - post-order traversal
+ * @tree: root node pointer
+ * @func: function
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if ((tree != NULL) && (func != NULL))
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
